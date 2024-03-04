610ebd9e66da0514d6c65ed4e235a61389dff7fa
chenmoonmo <chenmoonmo@gmail.com>
2024-03-04 15:33:12 +0800

    chore: user anchor

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,47 +1,18 @@
-"use client";
-import { FC, ReactNode, useMemo } from "react";
+import { FC, ReactNode } from "react";
 import { Theme } from "@radix-ui/themes";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  SolongWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-
-import "@radix-ui/themes/styles.css";
-import "@solana/wallet-adapter-react-ui/styles.css";
+import { SolanaProvider } from "@/components/solana-provider";
+import { ClusterProvider } from "@/components/cluster-data-access";
 
 export const Provider: FC<{
   children: ReactNode;
 }> = ({ children }) => {
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(
-    () =>
-      [
-        new SolongWalletAdapter(),
-        new PhantomWalletAdapter(),
-        new SolflareWalletAdapter(),
-      ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [network]
-  );
-
   return (
     <Theme appearance="dark" accentColor="blue">
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>{children}</WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
+      <ClusterProvider>
+        <SolanaProvider>
+          {children}
+          </SolanaProvider>
+      </ClusterProvider>
     </Theme>
   );
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletButton } from "./solana-provider";
 
 const Navs = [
   {
@@ -59,7 +59,7 @@ export const Header = () => {
         </div>
       </div>
       <div className="flex items-center gap-3">
-        <WalletMultiButton />
+        <WalletButton />
       </div>
     </header>
   );
