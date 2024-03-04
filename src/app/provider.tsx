3d16e1836b50c42e8334f0fbc60a3ef92c1c18f4
chenmoonmo <chenmoonmo@gmail.com>
2024-03-04 15:49:40 +0800

    feat: add react-query

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,18 +1,27 @@
-import { FC, ReactNode } from "react";
+"use client";
+import { FC, ReactNode, useState } from "react";
 import { Theme } from "@radix-ui/themes";
 import { SolanaProvider } from "@/components/solana-provider";
 import { ClusterProvider } from "@/components/cluster-data-access";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export const Provider: FC<{
   children: ReactNode;
 }> = ({ children }) => {
+  const [client] = useState(new QueryClient());
+
   return (
-    <Theme appearance="dark" accentColor="blue">
-      <ClusterProvider>
-        <SolanaProvider>
-          {children}
+    <QueryClientProvider client={client}>
+      <ReactQueryStreamedHydration>
+        <ClusterProvider>
+          <SolanaProvider>
+            <Theme appearance="dark" accentColor="blue">
+              {children}
+            </Theme>
           </SolanaProvider>
-      </ClusterProvider>
-    </Theme>
+        </ClusterProvider>
+      </ReactQueryStreamedHydration>
+    </QueryClientProvider>
   );
 };
